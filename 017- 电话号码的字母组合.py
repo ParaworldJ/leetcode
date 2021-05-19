a4aa989a553a6af1c13712ba270d1c6d60f34834
ParaworldJ <81695457+ParaworldJ@users.noreply.github.com>
2021-05-19 22:36:30 +0800

    Create 017- 电话号码的字母组合.py
    不知道倒地用哪种才是最合适的解法

---
+++ 017- 电话号码的字母组合.py
@@ -0,0 +1,27 @@
+from itertools import product
+def letterCombinations(digits):
+    DtoNumList = {
+        '2':['a','b','c'],
+        '3':['d','e','f'],
+        '4':['g','h','i'],
+        '5':['j','k','l'],
+        '6':['m','n','o'],
+        '7':['p','q','r','s'],
+        '8':['t','u','v'],
+        '9':['w','x','y','z']
+    }
+    use = []
+    letter = []
+    for i in range(len(digits)):
+        if digits[i] == '1' or digits[i] == '0' and digits[i] in use:
+            continue
+        else:
+            use = use + [digits[i]]
+            letter = letter + [DtoNumList[digits[i]]]
+    result = []
+    for i in use:
+        for k in product(*letter):
+            result = result + ["".join(k)]
+    result = list(set(result))
+    return result
+print(letterCombinations('112131'))

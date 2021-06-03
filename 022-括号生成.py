793b3372dfb821990b67292d81b4947f4718525f
ParaworldJ <81695457+ParaworldJ@users.noreply.github.com>
2021-06-03 16:38:20 +0800

    Create 022-括号生成.py
    常年没搞了，回溯都要想想，我其实对于这里的return 不是很有把握

---
+++ 022-括号生成.py
@@ -0,0 +1,35 @@
+from collections import Counter
+def generateParenthesis(n: int):
+    b = ''
+    a = []
+    if n == 0: return []
+    j = 2 * n
+    left(j - 1 , b + '(',a ,n)
+    return a
+def right(n:int,b:str,a:list,T:int):
+    # print(n,b,a)
+    if n == 1:
+        if Counter(b)['('] > T or Counter(b)['('] < Counter(b)[')']:
+            return None
+        else: 
+            a.append(b+')')
+    else:
+        if Counter(b)['('] >= Counter(b)[')']:
+            left(n - 1, b + '(', a,T)
+            right(n - 1, b + ')', a,T)
+        else:
+            return None
+def left(n:int,b:str,a:list,T:int):
+    # print(n,b,a)
+    if n == 1:
+        if Counter(b)['('] > T or Counter(b)['('] < Counter(b)[')']:
+            return None
+        else: 
+            a.append(b+')') 
+    else:
+        if Counter(b)['('] >= Counter(b)[')']:
+            left(n - 1, b + '(', a,T)
+            right(n - 1, b + ')', a,T)
+        else:
+            return None
+print(generateParenthesis(1))
